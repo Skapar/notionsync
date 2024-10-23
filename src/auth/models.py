fc4a0d51973479607314ac9acbe769dc0c255cba
Your New Username <yournewemail@example.com>
2024-10-24 04:06:41 +0500

    fix: move user to auth models

---
+++ src/auth/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import MetaData
+
+from app.models import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
