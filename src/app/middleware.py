3fae7f97d12feebf3029ae5a0a11f9677e4a67c3
Your New Username <yournewemail@example.com>
2024-10-24 05:41:54 +0500

    feat: implement base logger

---
+++ src/app/middleware.py
@@ -0,0 +1,8 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    def dispatch(self, request: Request, call_next):
+        response = call_next(request)
+        print(f"Request: {request.method} {request.url}")
+        return response
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from .app.middleware import LoggingMiddleware
 
 from .app.config import settings
 from .auth.auth import router as auth_router
 from .app.routes import router as api_router
 
 app = FastAPI()
+
+app.add_middleware(LoggingMiddleware)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(api_router)
